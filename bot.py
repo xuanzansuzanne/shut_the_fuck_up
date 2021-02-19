dbf49743ceca1f5ffbb75c0e7083069583e76825
Jan <jzalobinski4@gmail.com>
2021-02-19 15:34:42 -0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+
+
+TOKEN = 'ODEyMzY1ODczMDQxODk5NTcw.YC_srQ.E7cvyJW_H5f-hAA_Qkv3Io37HBE'  # TODO : move to env
+GUILD = 'Quatro Air'
+
+intents = discord.Intents().default()
+intents.members = True
+
+client = commands.Bot(command_prefix="-", intents=intents)
+
+
+@client.event
+async def on_ready():
+    print("I'm in, bitch")
+
+
+def role_check(user):
+    for role in user.roles:
+        if role.name == 'Officers':
+            return True
+    return False
+
+
+@client.command()
+# Mute everyone in same voice chat as command author
+async def m(ctx):
+    if role_check(ctx.author):
+        vc = discord.utils.get(ctx.guild.voice_channels, id=ctx.author.voice.channel.id)
+        for member in vc.members:
+            if not role_check(member):
+                await member.edit(mute=True)
+
+
+@client.command()
+# Unmute everyone on server
+async def u(ctx):
+    if role_check(ctx.author):
+        for voice_channel in ctx.guild.voice_channels:
+            vc = discord.utils.get(ctx.guild.voice_channels, id=voice_channel.id)
+            for member in vc.members:
+                await member.edit(mute=False)
+
+client.run(TOKEN)
+
